8deb5f0e851a7ee9afcc0b57eb7987973bfad379
Anton Stupnitskiy <avs4820@gmail.com>
2026-08-03 01:13:22 +0300

    Fix Railway health check

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,13 @@ URL configuration for config project.
 """
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import include, path
+
+
+def health_check(request):
+    return HttpResponse("ok")
 
 
 def root_redirect(request):
@@ -15,6 +20,7 @@ def root_redirect(request):
 
 
 urlpatterns = [
+    path("health/", health_check, name="health-check"),
     path("", root_redirect, name="root"),
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
